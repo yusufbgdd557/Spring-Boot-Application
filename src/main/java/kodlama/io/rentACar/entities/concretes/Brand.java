3150aeb151d43f409ac30a3ce8b45ac9e7d38689
Yusuf Bağdad <yusufbgdd557@gmail.com>
2023-11-03 18:16:48 +0300

    first push

---
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -0,0 +1,23 @@
+package kodlama.io.rentACar.entities.concretes;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name="brands")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+}
